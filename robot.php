cc810ed8ab8bfa0d223de628443604c411771df1
dbCreator Studios <dbcreator.studios@gmail.com>
2020-08-02 12:19:55 +0700

    NEW

--- robot.php
+++ robot.php
@@ -1,5 +1,12 @@
 <?php 
 
-echo "Hello Master";
+$token = "1283875359:AAGN5ztnpGFmT5vw4HV_l5GKJG99GkVRYYI";
+$url = "https://api.telegrarm.org/bot" . $token;
+$telegram = json_decode(file_get_contents("php://input"), TRUE);
+$chatid = $telegram['message']['chat']['id'];
+$message = $telegram['message']['text'];
+
+file_get_contents($url . "/sendMessage?chat_id=" . $chatid . "&text=Halo Master");
+
 
 ?>
