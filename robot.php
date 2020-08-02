54ad5ec54653980aa955936b6f77686107cdde62
dbcreator-studios <dbcreator.studios@gmail.com>
2020-08-02 14:57:47 +0700

    NEW

--- robot.php
+++ robot.php
@@ -30,35 +30,35 @@ function inlineKeyboard($menud , $chatid, $text) {
     return apiReq("sendmessage?chat_id=$chatid&text=$text&parse_mode=Markdown&reply_markup=$d2");
 }
 
-// if($message == "/start") {
-//     send($chatid, "Halo Master");
-// }
-// if($message == "/keyboard") {
-//     $keyboard = [
-//         ["Inline", "Inline2"],
-//         ["Inline3", "Inline4"],
-//     ];
-//     $key = array(
-//         "resize_keyboard" => true,
-//         "keyboard" => $keyboard,
-//     );
-//     keyboard($key, $chatid, "Halo Master");
-// }
-// if($message == "Inline") {
-//     $but = array(array(array("text" => "Button 1", "url" => "www.google.com"),),);
-//     inlineKeyboard($but, $chatid, "Inline");
-// }
+if($message == "/start") {
+    send($chatid, "Halo Master");
+}
+if($message == "/keyboard") {
+    $keyboard = [
+        ["Inline", "Inline2"],
+        ["Inline3", "Inline4"],
+    ];
+    $key = array(
+        "resize_keyboard" => true,
+        "keyboard" => $keyboard,
+    );
+    keyboard($key, $chatid, "Halo Master");
+}
+if($message == "Inline") {
+    $but = array(array(array("text" => "Button 1", "url" => "www.google.com"),),);
+    inlineKeyboard($but, $chatid, "Inline");
+}
 
-// if($message == "Inline2") {
-//     $but = array(array(array("text" => "Button 1", "url" => "www.google.com"), array("text" => "Button 2", "url" => "www.youtube.com"),),);
-//     inlineKeyboard($but, $chatid, "Inline2");
-// }
+if($message == "Inline2") {
+    $but = array(array(array("text" => "Button 1", "url" => "www.google.com"), array("text" => "Button 2", "url" => "www.youtube.com"),),);
+    inlineKeyboard($but, $chatid, "Inline2");
+}
 
-// if($message == "Inline3") {
-//     $but[] = array(array(array("text" => "Button 1", "url" => "www.google.com"),),);
-//     $but[] = array(array(array("text" => "Button 2", "url" => "www.google.com"),),);
-//     inlineKeyboard($but, $chatid, "Inline3");
-// }
+if($message == "Inline3") {
+    $but[] = array(array(array("text" => "Button 1", "url" => "www.google.com"),),);
+    $but[] = array(array(array("text" => "Button 2", "url" => "www.google.com"),),);
+    inlineKeyboard($but, $chatid, "Inline3");
+}
 
 // if($message == "Inline4") {
 //     $but = array(array(array("text" => "Button 1", "callback_data" => "ciao1"),),);
