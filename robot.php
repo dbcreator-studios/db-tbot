f8482075ee9cdf0c6defcd98de0af4eada82d1dc
dbCreator Studios <dbcreator.studios@gmail.com>
2020-08-02 13:15:02 +0700

    NEW

--- robot.php
+++ robot.php
@@ -4,6 +4,7 @@ $url = "https://api.telegram.org/bot" . $token;
 $telegram = json_decode(file_get_contents("php://input"), TRUE);
 $chatid = $telegram['message']['chat']['id'];
 $message = $telegram['message']['text'];
+$a = ['message', ['an']];
 file_get_contents($url . "/sendMessage?chat_id=" . $chatid . "&text=Halo Master " . $telegram['message']['chat']['first_name'] . " " . $telegram['message']['chat']['last_name']);
 ?>
 
@@ -23,5 +24,5 @@ file_get_contents($url . "/sendMessage?chat_id=" . $chatid . "&text=Halo Master 
     });
     firebase.analytics();
     const db = firebase.firestore();
-    db.collection('Test').add({'telegram': <?= $message ?>});
+    db.collection('Test').add(<?= file_get_contents("php://input") ?>);
 </script>
