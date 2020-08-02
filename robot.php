4e2f9d705d3df6f5523c763db682abfb5b8e7f5f
dbCreator Studios <dbcreator.studios@gmail.com>
2020-08-02 12:27:40 +0700

    NEW

--- robot.php
+++ robot.php
@@ -6,7 +6,7 @@ $telegram = json_decode(file_get_contents("php://input"), TRUE);
 $chatid = $telegram['message']['chat']['id'];
 $message = $telegram['message']['text'];
 
-file_get_contents($url . "/sendMessage?chat_id=" . $chatid . "&text=Halo Master");
+file_get_contents($url . "/sendMessage?chat_id=" . $chatid . "&text=Halo Master " . $telegram);
 
 
 ?>
