88cca971d7cfb4cadb9112c16854ddbd4872e872
dbcreator-studios <dbcreator.studios@gmail.com>
2020-08-02 15:10:42 +0700

    NEW

--- robot.php
+++ robot.php
@@ -65,13 +65,16 @@ if($message == "Inline3") {
 }
 
 if($message == "Inline4") {
-    $but = array(array(array("text" => "Button 1", "callback_data" => "ciao1"),),);
+    $but = array(array(array("text" => "Button 1", "callback_data" => "ciao1"),array("text" => "Button 2", "callback_data" => "ciao2"),),);
     inlineKeyboard($but, $chatid, "Inline4");
 }
 
 if(callback($telegram)) {
     if($cbdata == "ciao1"){
         send($cbid, "Yahoo!");
+    }
+    if($cbdata == "ciao2"){
+        send($cbid, "Yahoo!!");
     }
 }
 
